c147e8dd014688b1554a92efab5db70fc53ec981
Adrián Villaseco Soto <adrianvillasecosoto@gmail.com>
2021-05-21 19:20:02 +0200

    Creacion del proyecto back con spring boot

---
+++ clients-crud-back/src/main/java/com/demo/clients/crud/back/ClientsCrudBackApplication.java
@@ -0,0 +1,13 @@
+package com.demo.clients.crud.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsCrudBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsCrudBackApplication.class, args);
+	}
+
+}
