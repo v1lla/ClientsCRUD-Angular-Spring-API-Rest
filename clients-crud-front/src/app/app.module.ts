d0848553fccee2c3766199bac1adea053bd74ebc
Adrián Villaseco Soto <adrianvillasecosoto@gmail.com>
2021-05-22 12:02:57 +0200

    fix cors y uso de httpClient en angular

--- clients-crud-front/src/app/app.module.ts
+++ clients-crud-front/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClientesService } from './clientes/clientes.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,9 @@ import { ClientesService } from './clientes/clientes.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
+    
   ],
   providers: [ClientesService],
   bootstrap: [AppComponent]
--- clients-crud-front/src/app/clientes/clientes.service.ts
+++ clients-crud-front/src/app/clientes/clientes.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './cliente';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClientesService {
 
-  constructor() { }
+  private urlEndpointGetClients : string = 'http://localhost:8080/api/clients';
 
-  clientList : Cliente[] =  [
-    {id: 1234, name: "Adrian", surname: "Villaseco Soto", phone: "696332173", createAt: "10-05-2021"},
-    {id: 3333, name: "Pedro", surname: "Rodriguez", phone: "12345123", createAt: "10-05-2021"},
-    {id: 4444, name: "Romero", surname: "Fernandez", phone: "12312321", createAt: "10-05-2021"},
-    {id: 6232, name: "Damian", surname: "Sanchez", phone: "33434343", createAt: "10-05-2021"},
-    {id: 1231, name: "Sergio", surname: "Lopez Lopez", phone: "23232323", createAt: "10-05-2021"},
-    {id: 6121, name: "David", surname: "Blanco Gutierrez", phone: "123213121", createAt: "10-05-2021"}
-  ];
+  constructor( private http : HttpClient) { }
 
 
   getClientes(): Observable<Cliente[]> {
-    return of(this.clientList);
+    return this.http.get<Cliente[]>(this.urlEndpointGetClients);
   }
+  
 }
